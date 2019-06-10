4893ba475772b34baf3128fd6b1924980f6d801c
Patricia Parker <pk.patricia@gmail.com>
2019-06-10 18:03:11 -0500

    Lesson3PythonUnsolved

--- 01-Lesson-Plans/03-Python/3/Activities/01-Stu_CerealCleaner/Unsolved/cereal_unsolved.py
+++
@@ -1,4 +0,0 @@
-import os
-import csv
-
-cereal_csv = os.path.join("../Resources", "cereal.csv")
--- 01-Lesson-Plans/03-Python/3/Activities/07-Stu_Functions/Unsolved/main.py
+++
@@ -1,6 +0,0 @@
-# @TODO: Write a function that returns the arithmetic average for a list of numbers
-
-
-# Test your function with the following:
-# print(average([1, 5, 9]))
-# print(average(range(11)))
